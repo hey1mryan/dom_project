034c1321200b33f1b555054e88973457f4fc94dd
hey1mryan <ryancai7519@gmail.com>
2024-07-29 11:40:48 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Add your code to this file
+let bgColor = document.querySelector("body");
+bgColor.style.backgroundColor = "#00008b";
+
+let intro = document.querySelector("h1");
+intro.style.textDecoration = "underline";
+
+let startersImage = document.getElementById("starters_image_div")
+startersImage.style.borderColor = "#FFCB05";
+startersImage.style.borderStyle = "dashed";
+
+//im cooked LOL
